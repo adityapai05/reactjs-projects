091e497b25ae2eba39d6c9b3e65e96f1691021cf
Aditya Pai <139843545+adityapai05@users.noreply.github.com>
2024-10-26 18:15:31 +0530

    Delete Module 2 directory

--- Module 2/vite-project/src/components/Demo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Demo (props) { //Can also use destructuring of object to pass props, check Greet.jsx
-    return (
-        <p>{props.name} speaking from Demo component!</p>
-    )
-}
-
-export default Demo
